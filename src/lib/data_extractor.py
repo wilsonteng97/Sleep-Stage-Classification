c42eafc674c2a9fe8c9db410c5b59acc0c8da471
Eric Ma <eric.m.990909@gmail.com>
2020-11-07 12:27:32 +0800

    Update data extractor

--- src/lib/data_extractor.py
+++ src/lib/data_extractor.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy.signal import periodogram
 from scipy.special import digamma
 from scipy.stats import kurtosis, skew
 
@@ -49,6 +51,13 @@ class DataExtractor():
         df['std'] = self.calculateStandardDeviation(self.x)
         df['skew'] = self.calculateSkew(self.x)
         df['kurtosis'] = self.calculateKurtosis(self.x)
+        df['hjorth'] = self.calculateHjorth(self.x)
+        df['mmd'] = self.calculateMMD(self.x)
+        df['pfd'] = self.calculatePFD(self.x)
+        df['kfd'] = self.calculateKFD(self.x)
+        df['lrssv'] = self.calculateLRSSV(self.x)
+        df['se'] = self.calculateSE(self.x)
+        df['re'] = self.calculateRE(self.x)
 
         for i, band_name in enumerate(self.freq_band_dict.keys()):
             data = self.freq_band_dict[band_name]
@@ -56,6 +65,13 @@ class DataExtractor():
             df[band_name + 'STD'] = self.calculateStandardDeviation(data)
             df[band_name + 'Skew'] = self.calculateSkew(data)
             df[band_name + 'Kurtosis'] = self.calculateKurtosis(data)
+            df[band_name + 'Hjorth'] = self.calculateHjorth(data)
+            df[band_name + 'MMD'] = self.calculateMMD(data)
+            df[band_name + 'PFD'] = self.calculatePFD(data)
+            df[band_name + 'KFD'] = self.calculateKFD(data)
+            df[band_name + 'LRSSV'] = self.calculateLRSSV(data)
+            df[band_name + 'SE'] = self.calculateSE(data)
+            df[band_name + 'RE'] = self.calculateRE(data)
         return df
     
     def calculateMean(self, arr):
@@ -70,11 +86,10 @@ class DataExtractor():
     def calculateKurtosis(self, arr):
         return kurtosis(arr, axis=1)
     
-
-    def calculateHjorth(self):
-        first_deriv = np.diff(self.x, n=1, axis=1)
-        second_deriv = np.diff(self.x, n=2, axis=1)
-        var_zero = np.mean(self.x ** 2, axis=1)
+    def calculateHjorth(self, arr):
+        first_deriv = np.diff(arr, n=1, axis=1)
+        second_deriv = np.diff(arr, n=2, axis=1)
+        var_zero = np.mean(arr ** 2, axis=1)
         var_d1 = np.mean(first_deriv ** 2)
         var_d2 = np.mean(second_deriv ** 2)
         activity = var_zero
@@ -82,31 +97,33 @@ class DataExtractor():
         complexity = np.sqrt(var_d2 / var_d1) / morbidity
         return activity, morbidity, complexity
 
+    def calculateMMD(self, arr):
+        return np.max(arr, axis=1) - np.min(arr, axis=1)
 
-    def calculateMMD(self):
-        return np.max(self.x, axis=1) - np.min(self.x, axis=1)
-
-
-    def calculatePFD(self):
-        N = np.full((self.x.shape[0]), self.x.shape[1])
-        diff = np.diff(self.x, n=1, axis=1)
+    def calculatePFD(self, arr):
+        N = np.full((arr.shape[0]), arr.shape[1])
+        diff = np.diff(arr, n=1, axis=1)
         sign_change = (diff[:,1:-1] * diff[:,0:-2] < 0)
         M = sign_change.sum(axis=1)
         return np.log10(N) / (np.log10(N) + np.log10(N / (N + 0.4 * M)))
 
-
-    # TODO: WIP
-    def calculateKFD(self, a):
-        distance = np.abs(np.ediff1d(a))
-        LL = distance.sum() #'line length'
-        LL_normalized = np.log10(np.divide(LL, distance.mean())) #original paper uses constant hyperparameter M=1
-        aux_d = a - a[0]
-        d = np.max(np.abs(aux_d[1:]))
+    def calculateKFD(self, arr):
+        distance = np.abs(np.diff(arr, n=1, axis=1))
+        LL = distance.sum(axis=1)
+        LL_normalized = np.log10(np.divide(LL, distance.mean(axis=1)))
+        aux_d = arr- arr[:,0][:, None]
+        d = np.max(np.abs(aux_d[:,1:]), axis=1)
         return np.divide(LL_normalized, np.add(LL_normalized, np.log10(np.divide(d, LL))))
 
-
-    # TODO: WIP
+    # TODO: convert to 2D
     def calculateHurstExponent(self, a):
+        #Hurst Exponent
+        def getLags(arr_len):
+            if arr_len == 3000:
+                return [3000, 1500, 750, 250, 125, 60, 30, 15, 8, 4, 2]
+            else:
+                return [2**x for x in range(int(np.log2(arr_len)+1))][::-1][:-1]
+
         alen = len(a)
         if alen < 100:
             raise Error(f"Not enough datapoints to calculate Hurst Exp | Only {alen} of the required 100.")
@@ -135,33 +152,30 @@ class DataExtractor():
         m, c = np.polyfit(ln_lag_ls,ln_Rs_ls,1)
         return m
 
-
-    #Log Root Sum of Sequential Variations
-    #measure the sequential variations
-    def calculateLRSSV(self):
-        diff = np.diff(self.x, n=1, axis=1)
+    # Log Root Sum of Sequential Variations
+    # Measure the sequential variations
+    def calculateLRSSV(self, arr):
+        diff = np.diff(arr, n=1, axis=1)
         return np.log10(np.sqrt(np.sum(diff**2, axis=1)))
 
+    # Normalized Spectral Entropy, https://raphaelvallat.com/entropy/build/html/_modules/entropy/entropy.html
+    def calculateSE(self, arr):
+        def calculateSE1D(a):
+            _, psd = periodogram(a, 100)  #fft transform
+            psd_norm = np.divide(psd, psd.sum()) #power spectral density, measure of signal's power content versus frequency
+            se = -np.multiply(psd_norm, np.log2(psd_norm)).sum()
+            se /= np.log2(psd_norm.size)
+            return se
+        return np.apply_along_axis(calculateSE1D, 1, arr)
 
-    #Normalized Spectral Entropy, https://raphaelvallat.com/entropy/build/html/_modules/entropy/entropy.html
-    # TODO: WIP
-    def calculateSE(self, a):
-        _, psd = periodogram(a, 100)  #fft transform
-        psd_norm = np.divide(psd, psd.sum()) #power spectral density, measure of signal's power content versus frequency
-        se = -np.multiply(psd_norm, np.log2(psd_norm)).sum()
-        se /= np.log2(psd_norm.size)
-        return se
+    # Rényi entropy (RE) a measure of the entropy of the distribution P = (p1, p2,..., pn)
+    def calculateRE(self, arr, m=2):
+        return 1/(1-m) * np.log2(np.sum(arr**m, axis=1))
 
-
-    #Rényi entropy (RE) a measure of the entropy of the distribution P = (p1, p2,..., pn)
-    def calculateRE(self, m=2):
-        return 1/(1-m) * np.log2(np.sum(self.x**m, axis=1))
-
- 
-    #Kraskov entropy (KE) an estimate for Shannon entropy using N samples of an m-dimensional random vector x
-    #very confused???
-    # TODO: WIP
-    def calculateKE(self, a):
+    # Kraskov entropy (KE) an estimate for Shannon entropy using N samples of an m-dimensional random vector x
+    # very confused???
+    # TODO: Conver to 2D, using lambda takes a long time
+    def calculateKE1D(self, a):
         N = len(a)
         k = int(np.sqrt(N)) # sqrt of epoch len (3000)
         sum_ = 0
